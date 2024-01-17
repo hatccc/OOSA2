222c47804ffe22e2ec5b45ac9f513dc46d261512
Takuya Hattori <amatebmsphs@gmail.com>
2024-01-17 11:56:01 +0000

    task03 and task04 files added

---
+++ task03.py
@@ -0,0 +1,4 @@
+for y in range(1, 9):
+    for x in range(1,9):
+        print(f"({x}, {y})")
+
---
+++ task04.py
@@ -0,0 +1,13 @@
+from math import sqrt
+
+for y0 in range(1, 9):
+    for x0 in range(1, 9):
+        for y1 in range(1, 9):
+            for x1 in range(1, 9):
+                if not (x0==x1 and y0==y1):
+                    distance = sqrt( (x0-x1)**2 + (y0-y1)**2)
+                    sentence = "Distance between (" + str(x0) + ", " + str(y0) + ") and (" + str(x1) + ", " + str(y1) + ") is " + str(distance)
+                    print(sentence)
+                    #print(f."Distance between ({x0}, {y0}) and ({x1}, {y1}) is {distance}")
+
+
